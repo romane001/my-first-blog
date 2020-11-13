bea03c8de03e379a88874b6fb3537a4e15873219
paola <pa.alarcons@alumnos.duoc.cl>
2020-11-13 17:44:11 -0300

    Added views to create/edit blog post inside the site.

--- blog/templates/blog/forms.py
+++ blog/templates/blog/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.shortcuts import render, get_object_or_404
 
+from .models import Post
 
 class PostForm(forms.ModelForm):
 
     class Meta:
-        model = PostForm
+        model = Post
         fields = ('title', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
-def post_detail(request, pk):
+return redirect('post_detail', pk=post.pk)
+
+def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
